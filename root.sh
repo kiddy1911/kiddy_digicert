13960e06e72e97ff6b784341a9c9a6652bc41f8c
kiddy1911 <152975505+kiddy1911@users.noreply.github.com>
2025-01-16 22:10:15 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,7 +1,9 @@
+
+
 #!/bin/bash
 
 # 设置上海时区
-sudo timedatectl set-timezone Asia/Shanghai
+timedatectl set-timezone Asia/Shanghai
 
 # 获取当前日期
 current_date=$(date +%F)
@@ -9,25 +11,37 @@ current_date=$(date +%F)
 # 获取公网 IP 地址
 public_ip=$(curl -s -4 ifconfig.me)
 
-
 # 更新 root 密码
 echo "root:$current_date-$public_ip" | sudo chpasswd
 
-
 # 允许 root 用户通过 SSH 登录
-echo 'PermitRootLogin yes' | sudo tee -a /etc/ssh/sshd_config
+
+sed -i '/PermitRootLogin/d' /etc/ssh/sshd_config
+echo "PermitRootLogin yes" | sudo tee -a /etc/ssh/sshd_config
+
 
 # 启用密码认证
-sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+sed -i '/PasswordAuthentication/d' /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" | sudo tee -a /etc/ssh/sshd_config
+
 
 # 去掉注释的 Include 行
-sudo sed -i 's/^Include/#Include/' /etc/ssh/sshd_config
+sed -i 's/^Include/#Include/' /etc/ssh/sshd_config
 
 
 # 重启 SSH 服务
-sudo service sshd restart
 
-sudo systemctl restart sshd
+service sshd restart
+
+systemctl restart sshd
+
+setenforce 0
+
+sed -i 's/^SELINUX=enforcing/SELINUX=permissive/' /etc/selinux/config
+
 
 # 设置曼谷时区
-sudo timedatectl set-timezone Asia/Bangkok
+timedatectl set-timezone Asia/Bangkok
+
+
